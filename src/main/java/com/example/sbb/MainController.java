38c476e1b17fb759e161e948a0d4a78c3123ca86
tjqls <tjqls2013@gmail.com>
2023-05-02 13:25:54 +0900

    study1

--- src/main/java/com/example/sbb/MainController.java
+++ src/main/java/com/example/sbb/MainController.java
@@ -3,28 +3,47 @@ package com.example.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/name")
-    @ResponseBody
-    public String name(){
-    return "서빈";
-}
+    int count =0;
 
-    @RequestMapping("/age")
+    @GetMapping("/plus")
     @ResponseBody
-    public int age(){
-        return 26;
+    public int showPlus(int a, int b){
+        return a+b;
     }
 
-    @GetMapping("/height")
+    @GetMapping("/minus")
     @ResponseBody
-    public int height(){
-        return 170;
+    public int minus(int a, int b){
+        return a-b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int increase(){
+        return count++;
+    }
+
+    @GetMapping("/mbti/{name}")
+    @ResponseBody
+    public String showMbti(@PathVariable String name){
+        return switch (name) {
+            case "홍길동" -> "INFP";
+            case "홍길순","임꺽정" -> "ENFP";
+            case "반장" -> "ESTJ";
+            case "서빈" -> {
+                char P = 'P';
+                yield  "ISF" + P;
+            }
+            default -> "모름";
+
+        };
     }
 
 }
