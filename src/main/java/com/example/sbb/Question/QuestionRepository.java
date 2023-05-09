07a5d5e0b9be2dc64e55b1f2975e18dac2a5671d
깃tjqls <tjqls2013@gmail.com>
2023-05-09 11:40:19 +0900

    last

--- src/main/java/com/example/sbb/Question/QuestionRepository.java
+++ src/main/java/com/example/sbb/Question/QuestionRepository.java
@@ -1,6 +1,9 @@
 package com.example.sbb.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
@@ -8,4 +11,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/example/sbb/Question/QuestionService.java
+++ src/main/java/com/example/sbb/Question/QuestionService.java
@@ -3,9 +3,14 @@ package com.example.sbb.Question;
 
 import com.example.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,6 +39,13 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
 }
